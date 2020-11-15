96ae0cd392504968b2f884ce982e29dd084fbc80
fahdgdo <fahd_gdo@yahoo.com>
2020-11-16 01:38:07 +0300

    Add Bootstrap Templet

---
+++ products/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .views import products_list, product_details,product_add,product_edit
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+
+
+urlpatterns = [
+    path('products/', products_list, name='products_list'),
+    path('products/add', product_add, name='product_add'),
+    path('products/<pk>', product_details, name='product_details'),
+    path('products/edit/<pk>', product_edit, name='product_edit')
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
