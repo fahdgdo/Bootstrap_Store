54e21d30f73a15e3f3d229d57069717d504b1ffb
fahdgdo <fahd_gdo@yahoo.com>
2020-12-23 05:31:36 +0300

    Add Default Email

--- store/prod_settings.py
+++ store/prod_settings.py
@@ -6,7 +6,12 @@ from .settings import *
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-DEBUG = False
+debg_option = os.environ.get('DEBUG').lower
+if debg_option == 'true':
+    DEBUG = True
+else:
+    DEBUG = False
+
 
 ALLOWED_HOSTS = ['peaceful-escarpment-54364.herokuapp.com']
 
@@ -14,3 +19,13 @@ ALLOWED_HOSTS = ['peaceful-escarpment-54364.herokuapp.com']
 DATABASES = {
     'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = 'apikey'  # this is exactly the value 'apikey'
+EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
