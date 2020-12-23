a029d289cb611eefd6aec47b8ba4e0d2b40bd9a3
fahdgdo <fahd_gdo@yahoo.com>
2020-12-23 04:08:33 +0300

    Add Database Config

--- store/prod_settings.py
+++ store/prod_settings.py
@@ -1,5 +1,7 @@
 import os
 
+import dj_database_url
+
 from .settings import *
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
@@ -7,3 +9,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = ['peaceful-escarpment-54364.herokuapp.com']
+
+
+DATABASES = {
+    'default': dj_database_url.config(os.environ.get('DATABASE_URL'))
+}
